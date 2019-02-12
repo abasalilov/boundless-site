84c68ccb09864f6a1bce6351a51496cea71bc8fb
abasalilov <abasalilov@gmail.com>
2019-02-11 23:59:58 -0800

    updated

---
+++ one.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+const listOfClassNames = [];
+const chop = str => {
+  return str.slice(0, str.indexOf(`"`));
+};
+
+axios.get("http://localhost:4000/coaching/").then(res => {
+  const arr = res.data.split(`class="`);
+  const filtered = arr.map(word => chop(word));
+  console.log("filtered", filtered);
+});
